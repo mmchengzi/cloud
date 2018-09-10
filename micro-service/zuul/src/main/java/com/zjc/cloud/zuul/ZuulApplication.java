070e671c79f3d654c4c68eed8cf3b889933fb291
zjc <1173727598@qq.com>
2018-09-10 12:15:50 +0800

    updata

--- micro-service/zuul/src/main/java/com/zjc/cloud/zuul/ZuulApplication.java
+++ micro-service/zuul/src/main/java/com/zjc/cloud/zuul/ZuulApplication.java
@@ -2,9 +2,24 @@ package com.zjc.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-public class ZuulApplication {
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ZuulApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(ZuulApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
