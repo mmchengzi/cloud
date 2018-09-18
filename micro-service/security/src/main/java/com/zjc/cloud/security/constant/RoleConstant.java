80d9c963008225c05ebd47ce97702d746cbcc866
zjc <1173727598@qq.com>
2018-09-18 17:46:16 +0800

    add

---
+++ micro-service/security/src/main/java/com/zjc/cloud/security/constant/RoleConstant.java
@@ -0,0 +1,5 @@
+package com.zjc.cloud.security.constant;
+
+public interface RoleConstant {
+    String ROLE_USER = "ROLE_USER";
+}
---
+++ micro-service/security/src/main/java/com/zjc/cloud/security/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.zjc.cloud.security.mapper;
+
+
+import com.zjc.cloud.security.entity.UserEntity;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component
+public interface UserMapper {
+
+    @Insert("insert into user(username, password, nickname, roles) values(#{username}, #{password}, #{nickname}, #{roles})")
+    int insert(UserEntity userEntity);
+
+    @Select("select * from user where username = #{username}")
+    UserEntity selectByUsername(@Param("username") String username);
+
+}
