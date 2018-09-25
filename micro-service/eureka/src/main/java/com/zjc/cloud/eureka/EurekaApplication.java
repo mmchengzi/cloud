929c691721b53ede595030c8ad6e636e48eb9c47
zjc <1173727598@qq.com>
2018-09-25 17:07:02 +0800

    add

--- micro-service/eureka/src/main/java/com/zjc/cloud/eureka/EurekaApplication.java
+++ micro-service/eureka/src/main/java/com/zjc/cloud/eureka/EurekaApplication.java
@@ -2,26 +2,18 @@ package com.zjc.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
-*User zjc
-*Created with IntelliJ IDEA
-*Created on 2018-09-10 10:32
-*
-*/
+ * User zjc
+ * Created with IntelliJ IDEA
+ * Created on 2018-09-10 10:32
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication extends SpringBootServletInitializer {
+public class EurekaApplication{
 
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder application) {
-		return application.sources(EurekaApplication.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+		public static void main(String[] args) {
+			SpringApplication.run(EurekaApplication.class, args);
+		}
 }
