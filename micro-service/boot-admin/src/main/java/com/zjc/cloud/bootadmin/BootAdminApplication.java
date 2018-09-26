4505c79870faa2c1c1e3538ba6aac19e96e3add3
zjc <1173727598@qq.com>
2018-09-26 09:52:31 +0800

    updata

--- micro-service/boot-admin/src/main/java/com/zjc/cloud/bootadmin/BootAdminApplication.java
+++ micro-service/boot-admin/src/main/java/com/zjc/cloud/bootadmin/BootAdminApplication.java
@@ -3,22 +3,26 @@ package com.zjc.cloud.bootadmin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
+@SpringBootApplication
 @EnableAdminServer
+@EnableEurekaClient
 public class BootAdminApplication extends SpringBootServletInitializer {
+
 	@Override
 	protected SpringApplicationBuilder configure(
 			SpringApplicationBuilder application) {
 		return application.sources(BootAdminApplication.class);
 	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BootAdminApplication.class, args);
 	}
